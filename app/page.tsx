69f922f2b6605999cca093938feaab503d6fe928
Junior Dering <juniordering@hotmail.com>
2023-08-12 15:12:13 -0400

    feat: parallax animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,43 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import stars from "@/app/assets/stars.png";
 import moon from "@/app/assets/moon.png";
 import mountains_front from "@/app/assets/mountains_front.png";
 import mountains_behind from "@/app/assets/mountains_behind.png";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    if (window != undefined) {
+      const stars = document.querySelector("#stars") as HTMLElement;
+      const moon = document.querySelector("#moon") as HTMLElement;
+      const mountains_behind = document.querySelector(
+        "#mountains_behind",
+      ) as HTMLElement;
+      const text = document.querySelector("#text") as HTMLElement;
+      const btn = document.querySelector("#btn") as HTMLElement;
+      const header = document.querySelector(
+        "header",
+      ) as HTMLElement;
+
+      window.addEventListener("scroll", () => {
+        const value = window.scrollY;
+        stars.style.left = value * 0.25 + "px";
+        moon.style.top = value * 1.05 + "px";
+        mountains_behind.style.top = value * 0.5 + "px";
+        text.style.transform = `translate(${(-value + 300) * 2.5}px, ${
+          (-96 + value) * 0.5
+        }px)`;
+        btn.style.transform = `translate(0px, ${96 + value * 0.5}px)`;
+        header.style.top = value * 0.5 + "px";
+      });
+    }
+  }, []);
+
   return (
-    <main className='min-h-screen bg-gradient-body'>
+    <main className='min-h-screen bg-gradient-body overflow-x-hidden'>
       <header className='absolute top-0 left-0 z-50 flex justify-between items-center w-full py-7 px-24 '>
         <Link
           href='#'
@@ -32,8 +62,10 @@ export default function Home() {
           </li>
         </ul>
       </header>
-      <section className='relative flex justify-center items-center w-full h-screen p-24 before:content-[""] before:absolute before:bottom-0 
-      before:w-full before:h-24 before:z-50 before:bg-gradient-before '>
+      <section
+        className='relative flex justify-center items-center w-full h-screen p-24 before:content-[""] before:absolute before:bottom-0 
+      before:w-full before:h-24 before:z-50 before:bg-gradient-before overflow-hidden '
+      >
         <Image src={stars} alt='stars' id='stars' />
         <Image src={moon} alt='moon' id='moon' className='mix-blend-screen' />
         <Image
@@ -44,17 +76,18 @@ export default function Home() {
 
         <h2
           id='text'
-          className='absolute text-white whitespace-nowrap text-[7.5vw] z-10'
+          className='absolute text-white whitespace-nowrap text-[7.5vw] z-10 -translate-x-full'
         >
           Moon Light
         </h2>
 
-        <Link
-          href='#'
+        <a
+          href='#sec'
+          id='btn'
           className='inline-block py-2 px-7 rounded-2xl bg-white text-[#2b1055] text-[1.5em] z-10 translate-y-24 '
         >
           Explore
-        </Link>
+        </a>
 
         <Image
           src={mountains_front}
@@ -63,11 +96,11 @@ export default function Home() {
           className='z-20'
         />
       </section>
-      <div className='relative p-24 bg-[#1c0522]'>
+      <div id='sec' className='relative p-24 bg-[#1c0522]'>
         <h2 className='text-[3.5em] mb-2.5 text-white'>
           Parallax Scrolling Effects
         </h2>
-        <p className='text-[1.2em] text-white'>
+        <p className='text-[1.2em] text-white text-justify'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa harum
           sint tempore perferendis ducimus, eum, veniam in cupiditate quidem
           odit laborum ipsam quo minima rerum. Quia dolore tempore magni hic.
